86d3bbf1b41e4db29535f07154dc0f0d0c433143
s31w4n <sewan.khalni@gmail.com>
2022-12-29 19:54:38 +0330

    Update the slideShow

--- slideShow.js
+++ slideShow.js
@@ -15,7 +15,6 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -25,9 +24,5 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
 }
