a1a1aabf75e4b4f81d71f39e2a4009416b0dc7d4
s31w4n <sewan.khalni@gmail.com>
2023-01-25 12:48:43 +0330

    modify some style changes

--- slideShow.js
+++ slideShow.js
@@ -8,7 +8,6 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-  let i;
   let slides = document.getElementsByClassName("mySlides");
   if (n > slides.length) {
     slideIndex = 1;
@@ -16,7 +15,7 @@ function showSlides(n) {
   if (n < 1) {
     slideIndex = slides.length;
   }
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slides[slideIndex - 1].style.display = "block";
