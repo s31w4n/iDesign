242f9d0dd58954687bbf6fdddf2300d9301e98eb
s31w4n <63353528+s31w4n@users.noreply.github.com>
2023-01-26 00:50:23 +0330

    Update slideShow.js

--- slideShow.js
+++ slideShow.js
@@ -26,5 +26,5 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   // Schedule a new timeout.
-  timeOutId = setTimeout(showSlides, 2000); // Change image every 8 seconds
+  timeOutId = setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
